323c0af9e5d9c5f40521a2263d37c0513f2d6e4b
Alexey I. Churkin <phoebus@osiacat.ru>
2009-11-01 17:26:25 +0200

    + exteptions now sent to email by default inside front controller

--- etc/config.php
+++ etc/config.php
@@ -5,7 +5,14 @@
  *
  * **********************************************************************************************
  *
- * Copyright notice
+ * Copyright (c) 2009 phoebius.org
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation;
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with
+ * this program; if not, see <http://www.gnu.org/licenses/>.
  *
  ************************************************************************************************/
 
@@ -18,5 +25,7 @@ set_include_path(
 	. APP_ROOT .DIRECTORY_SEPARATOR . 'var' . DIRECTORY_SEPARATOR . 'lib' . PATH_SEPARATOR
 	. get_include_path()
 );
+
+define('BUGS_EMAIL', 'bugs@host.com');
 
 ?>
--- www/index.php
+++ www/index.php
@@ -5,7 +5,14 @@
  *
  * **********************************************************************************************
  *
- * Copyright notice
+ * Copyright (c) 2009 phoebius.org
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation;
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with
+ * this program; if not, see <http://www.gnu.org/licenses/>.
  *
  ************************************************************************************************/
 
@@ -34,17 +41,34 @@ try
 	// your stuff goes here
 	//
 }
-catch (ExecutionContextException $e)
+
+catch (Exception $e)
 {
-	echo '<pre>', $e->getTraceAsString();
-	throw $e;
-}
-catch (CompilationContextException $e)
-{
-	// this could happen at development and test hostConfigurations only
-	// and never in production system
-	echo '<pre>', $e->getTraceAsString();
-	throw $e;
+	if (APP_SLOT_CONFIGURATION & SLOT_CONFIGURATION_FLAG_DEVELOPMENT) {
+		echo '<pre>', $e->getTraceAsString();
+		throw $e;
+	}
+	else if (defined('BUGS_EMAIL')) {
+		mail(
+			BUGS_EMAIL,
+			"[Phoebius crash] {$_SERVER['HTTP_HOST']} crash #" . substr(sha1($e->getMessage()), 0, 6),
+			join(
+				"\n\n",
+				array(
+					$_SERVER['REQUEST_URI'],
+					'Request method: ' . $_SERVER['REQUEST_METHOD'],
+					get_class($e). ' msg: ' . $e->getMessage(),
+					print_r((array)$e, true),
+					$e->getTraceAsString()
+				)
+			)
+		);
+
+//		$chainedRouteContext->handle(
+//			$routingPolicy->getRule('404')->rewrite($webContext->getRequest(), new Route()),
+//			$webContext
+//		);
+	}
 }
 
 ?>
