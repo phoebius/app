b3cd49f528be8239f482845040b7328fd93dd15b
Alexey I. Churkin <phoebus@osiacat.ru>
2009-11-16 17:02:37 +0200

    + application initializer wrapper

--- lib/StandaloneSiteApplication.class.php
+++ lib/StandaloneSiteApplication.class.php
@@ -16,26 +16,18 @@
  *
  ************************************************************************************************/
 
-define('APP_ROOT', join(
-		DIRECTORY_SEPARATOR,
-		array_slice(
-			explode(DIRECTORY_SEPARATOR, dirname(__FILE__)), 0, -1
-		)
-	)
-);
-
-require ( APP_ROOT . '/phoebius/etc/app.init.php' );
-require ( APP_ROOT . '/etc/config.php' );
-
-//////////////////////////////////////////////////////////////////////////////////////////////////
-
-require
-	APP_ROOT . DIRECTORY_SEPARATOR .
-	'cfg' . DIRECTORY_SEPARATOR .
-	APP_SLOT . DIRECTORY_SEPARATOR .
-	'config.php';
-
-$application = new StandaloineSiteApplication();
-$application->run();
+/**
+ * @ingroup App_Web_Routing
+ */
+class StandaloneSiteApplication extends SiteApplication
+{
+	function __construct()
+	{
+		$router = new ChainedRouter;
+		
+		// TODO: fill the router here
+		
+		parent::__construct($router);
+	}
 
 ?>
